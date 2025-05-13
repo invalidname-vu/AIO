47c4a55b638a64687c1ef12d7fe8ec7c571741f6
invalidname-vu <68509631+invalidname-vu@users.noreply.github.com>
2025-05-13 08:39:48 +0700

    Add files via upload

---
+++ UI/banners/SetupBanners.js
@@ -0,0 +1,8 @@
+const setupBanners = {
+    helpBanner : 'https://cdn.discordapp.com/attachments/1095541203417706610/1371541484523884675/ezgif.com-animated-gif-maker.gif?ex=6823830b&is=6822318b&hm=fb5c5172393443c63b8a5759605ee5388f1af13894a357d1ca86dc5ba3fd9841&',
+    verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
+    ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
+    autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
+};
+
+module.exports = setupBanners;
